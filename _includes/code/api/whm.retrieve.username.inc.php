7a72ab0941e8bdca979b919077ec5c2fc805808c
Greg Chetcuti <greg@chetcuti.com>
2013-03-19 02:49:35 -0700

    Update license information

--- _includes/code/api/whm.retrieve.username.inc.php
+++ _includes/code/api/whm.retrieve.username.inc.php
@@ -1,4 +1,21 @@
 <?php
+// whm.retrieve.username.inc.php
+// 
+// WHM Data Warehouse - A Data Warehouse for WHM (Web Host Manager), as well as cPanel, written in PHP & MySQL.
+// Copyright (C) 2010 Greg Chetcuti
+// 
+// WHM Data Warehouse is free software; you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation; either version 2 of the License, or (at
+// your option) any later version.
+// 
+// WHM Data Warehouse is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+// License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with WHM Data Warehouse. If not,
+// please see http://www.gnu.org/licenses/
+?>
+<?php
 // RETRIEVE ACCOUNT USERNAME BASED ON DOMAIN NAME
 // input: $newdomain (make sure the domain is stored in $newdomain before executing this include file)
 // output: $newusername (after this include runs, $newusername will contain the domain's account username)
--- cron/01.build.dw.accounts.inc.php
+++ cron/01.build.dw.accounts.inc.php
@@ -1,4 +1,21 @@
 <?php
+// 01.build.dw.accounts.inc.php
+// 
+// WHM Data Warehouse - A Data Warehouse for WHM (Web Host Manager), as well as cPanel, written in PHP & MySQL.
+// Copyright (C) 2010 Greg Chetcuti
+// 
+// WHM Data Warehouse is free software; you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation; either version 2 of the License, or (at
+// your option) any later version.
+// 
+// WHM Data Warehouse is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+// License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with WHM Data Warehouse. If not,
+// please see http://www.gnu.org/licenses/
+?>
+<?php
 $sql = "DROP TABLE IF EXISTS _dw_whm_accounts";
 $result = mysql_query($sql,$connection) or die(mysql_error());
 
